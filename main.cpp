fc1eacca43c9105aa7411a0ad05c50c381c41735
Vloneskorpion <kamilnowak432@gmail.com>
2023-02-25 17:52:48 +0100

    Window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//Logging libs
-#include "easylogging++.h"
-INITIALIZE_EASYLOGGINGPP
-
 //OpenGl libs
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -16,13 +12,43 @@ INITIALIZE_EASYLOGGINGPP
 #include <math.h>
 
 //Includes
+#include "Logger.hpp"
 
 int main()
 {
-    //Logger
-    el::Configurations loggerConfiguration("../dependencies/easylogging/configuration.conf");
-    el::Loggers::reconfigureAllLoggers(loggerConfiguration);
+    Logger logger{"../dependencies/easylogging/configuration.conf"};
 
-    LOG(INFO) << "WORKING!";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
+    if(window == NULL)
+    {
+        LOG(ERROR) << "Couldn't create GLFW window";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        LOG(ERROR) << "Failed to initialize GLAD";
+        return -1;
+    }
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
